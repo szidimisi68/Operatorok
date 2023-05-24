4aca233918314f2d0d554c7154a9852c9eae0432
szidimisi68 <misikek5@gmail.com>
2023-05-24 17:15:47 +0200

    Add project files.

---
+++ Muvelet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Muvelet
+    {
+        double elsoSzam;
+        string jel;
+        double masodikSzam;
+
+        public Muvelet(string adatSor)
+        {
+            string[] felbontottSor = adatSor.Split(" ");
+            this.elsoSzam = double.Parse(felbontottSor[0]);
+            this.jel = felbontottSor[1];
+            this.masodikSzam = double.Parse(felbontottSor[2]);
+        }
+
+
+        public double ElsoSzam { get => elsoSzam; set => elsoSzam = value; }
+        public string Jel { get => jel; set => jel = value; }
+        public double MasodikSzam { get => masodikSzam; set => masodikSzam = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp7;
+using System;
+
+namespace ConsoleApp7 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Muvelet> muveletek = new List<Muvelet>(); 
+
+            File.ReadAllLines("kifejezesek.txt").ToList().ForEach(x => muveletek.Add(new Muvelet(x)));
+            Console.WriteLine(muveletek);
+            Console.WriteLine($"2.feladat: Kifejezések száma: {muveletek.Count()}");
+            Console.WriteLine($"3.feladat: Kifejezések maradékos osztással: {muveletek.Count(x => x.Jel == "mod")}");
+            Console.Write("4.feladat: ");
+            Console.WriteLine(muveletek.Any(x => x.ElsoSzam % 10 == 0 && x.MasodikSzam % 10 == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés.");
+            Console.WriteLine("5.feladat: Statisztika");
+            List<string> keresettJelek = new List<string> {"mod", "/", "div", "-", "*", "+"};
+            muveletek.Where(x => keresettJelek.Contains(x.Jel)).GroupBy(x => x.Jel).ToList().ForEach(x => Console.WriteLine($"{x.Key} -> {x.Count()} db".PadLeft(20)));
+        }
+    }
+}
